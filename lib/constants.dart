543378b99d31bf544b83864d993dc737f011bc7f
Billal Fauzan <billal.xcode@gmail.com>
2024-07-06 14:15:39 +0700

    init: setup theme color

---
+++ lib/constants.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class WappstikPalette {
+  static Color green        = const Color.fromARGB(255, 69, 170, 74);
+  static Color grey         = const Color.fromARGB(255, 242, 242, 242);
+  static Color grey200      = const Color.fromARGB(200, 242, 242, 242);
+  static Color red          = const Color(0xffff625b);
+  static Color surface      = const Color(0xfff4f8fb);
+  static Color bgCard1      = const Color(0xffe8edf1);
+  static Color bgBottomBar  = const Color(0xffc9c8d5);
+  static Color textGrey     = const Color(0xff78818b);
+}
