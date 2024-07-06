e0d16af50a464bca1410466f4d6e8f9a880b5b77
Billal Fauzan <billal.xcode@gmail.com>
2024-07-06 17:11:06 +0700

    feat(fonts): add default font size

--- lib/themes.dart
+++ lib/themes.dart
@@ -10,61 +10,76 @@ final ThemeData themeData = ThemeData.light().copyWith(
       displayLarge: TextStyle(
           fontFamily: 'Maison Neue',
           fontWeight: FontWeight.bold,
+          fontSize: 96,
           color: WappstikPalette.textGrey),
       displayMedium: TextStyle(
           fontFamily: 'Maison Neue',
           fontWeight: FontWeight.bold,
+          fontSize: 60,
           color: WappstikPalette.textGrey),
       displaySmall: TextStyle(
           fontFamily: 'Maison Neue',
           fontWeight: FontWeight.bold,
+          fontSize: 48,
           color: WappstikPalette.textGrey),
       headlineLarge: TextStyle(
           fontFamily: 'Maison Neue',
           fontWeight: FontWeight.bold,
+          fontSize: 40,
           color: WappstikPalette.textGrey),
       headlineMedium: TextStyle(
           fontFamily: 'Maison Neue',
+          fontSize: 34,
           fontWeight: FontWeight.bold,
           color: WappstikPalette.textGrey),
       headlineSmall: TextStyle(
           fontFamily: 'Maison Neue',
           fontWeight: FontWeight.bold,
+          fontSize: 24,
           color: WappstikPalette.textGrey),
       titleLarge: TextStyle(
           fontFamily: 'Maison Neue',
           fontWeight: FontWeight.bold,
+          fontSize: 20,
           color: WappstikPalette.textGrey),
       titleMedium: TextStyle(
           fontFamily: 'Maison Neue',
           fontWeight: FontWeight.normal,
+          fontSize: 16,
           color: WappstikPalette.textGrey),
       titleSmall: TextStyle(
           fontFamily: 'Maison Neue',
           fontWeight: FontWeight.normal,
+          fontSize: 14,
           color: WappstikPalette.textGrey),
       bodyLarge: TextStyle(
           fontFamily: 'Maison Neue',
           fontWeight: FontWeight.normal,
+          fontSize: 16,
           color: WappstikPalette.textGrey),
       bodyMedium: TextStyle(
           fontFamily: 'Maison Neue',
           fontWeight: FontWeight.normal,
+          fontSize: 14,
           color: WappstikPalette.textGrey),
       bodySmall: TextStyle(
           fontFamily: 'Maison Neue',
           fontWeight: FontWeight.normal,
+          fontSize: 12,
           color: WappstikPalette.textGrey),
       labelLarge: TextStyle(
           fontFamily: 'Maison Neue',
           fontWeight: FontWeight.normal,
+          fontSize: 14,
           color: WappstikPalette.textGrey),
       labelMedium: TextStyle(
           fontFamily: 'Maison Neue',
           fontWeight: FontWeight.normal,
+          fontSize: 12,
           color: WappstikPalette.textGrey),
       labelSmall: TextStyle(
           fontFamily: 'Maison Neue',
           fontWeight: FontWeight.normal,
+          fontSize: 10,
           color: WappstikPalette.textGrey),
     ));
