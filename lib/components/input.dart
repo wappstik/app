51dfd6fa2d07b0c72821e2d905bdd146023d9378
Billal Fauzan <billal.xcode@gmail.com>
2024-07-07 07:42:42 +0700

    feat(login): add input component

---
+++ lib/components/input.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:wappstik/constants.dart';
+
+class InputComponent extends StatelessWidget {
+  final IconData icon;
+  final String hintText;
+  final TextEditingController controller;
+  final TextInputType keyboardType;
+  final TextInputAction textInputAction;
+  final bool isSecure;
+  final bool hasSuffix;
+  final IconButton? suffixAction;
+
+  const InputComponent(
+      {super.key,
+      required this.icon,
+      required this.hintText,
+      required this.controller,
+      this.keyboardType = TextInputType.text,
+      this.textInputAction = TextInputAction.send,
+      this.isSecure = false,
+      this.hasSuffix = false,
+      this.suffixAction});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+        obscureText: isSecure,
+        style: Theme.of(context).textTheme.bodyMedium,
+        keyboardType: keyboardType,
+        textInputAction: TextInputAction.next,
+        decoration: InputDecoration(
+            suffixIcon: hasSuffix ? suffixAction : null,
+            prefixIcon: Icon(
+              icon,
+              color: WappstikPalette.grey,
+            ),
+            enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(width: 1, color: WappstikPalette.grey)),
+            focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(width: 1, color: WappstikPalette.grey)),
+            hintText: hintText,
+            hintStyle: Theme.of(context).textTheme.bodyMedium));
+  }
+}
