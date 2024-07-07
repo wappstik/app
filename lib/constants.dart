76936f294b4c0890692cca84e009133f3107458b
Billal Fauzan <billal.xcode@gmail.com>
2024-07-07 07:42:17 +0700

    feat(login): add new wappstik color palette

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class WappstikPalette {
   static Color green = const Color.fromARGB(255, 69, 170, 74);
-  static Color grey = const Color.fromARGB(255, 242, 242, 242);
+  static Color grey = const Color.fromARGB(255, 186, 186, 186);
   static Color grey200 = const Color.fromARGB(200, 242, 242, 242);
   static Color red = const Color(0xffff625b);
   static Color surface = const Color(0xfff4f8fb);
