6a1bbee857f7bd9e20079e2dd3930579d996f1a4
Billal Fauzan <billal.xcode@gmail.com>
2024-07-08 20:08:54 +0700

    ref(login): add auth service

--- lib/service/auth.dart
+++ lib/service/auth.dart
@@ -4,19 +4,25 @@ import 'package:http/http.dart' as http;
 import 'package:wappstik/constants.dart';
 
 class AuthService {
-  const AuthService();
+  AuthService();
+
+  final Map<String, String> headers = {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
+  };
+
+  Future<http.Response> login(String email, String password) async {
+    final body = jsonEncode({'email': email, 'password': password});
+    String url = '$baseUrl/login';
+    http.Response response =
+        await http.post(Uri.parse(url), body: body, headers: headers);
+    return response;
+  }
 
   Future<http.Response> register(
       String name, String email, String password) async {
-    debugPrint('Name: $name');
-    debugPrint('Email: $email');
-    debugPrint('Password: $password');
-    String url = '$baseurl/register';
-    debugPrint(url);
-    Map<String, String> headers = {
-      'Content-Type': 'application/json',
-      'Accept': 'application/json'
-    };
+    String url = '$baseUrl/register';
+
     final body =
         jsonEncode({'name': name, 'email': email, 'password': password});
 
